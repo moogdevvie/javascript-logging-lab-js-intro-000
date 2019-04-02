c0f809b1236ccbe8aa3db1c3f0ca451585e8a02e
Megan Choi <mchoi912@ucla.edu>
2019-04-02 22:50:17 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -32,7 +32,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.warn()', done => {
+  it('calls console.warn("are you sure you would like to proceed?")', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
 
     jsdom.env(html, [src], {
